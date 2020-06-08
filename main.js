a8b90405e612924fc9f29cee305f78e335da8a6d
BZdeny <BZdeny@users.noreply.github.com>
2020-06-09 02:43:55 +0700

    git add .

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+'use strict'
 dateInput('date')
-
 function dateInput(n) {
     const dateHere = new Date()
     let dateElement = document.getElementById(n)
@@ -13,12 +13,17 @@ function monthDate(date) {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     return months[date.getMonth()]
 }
-
-function enterKey() {
+(function enterKey() {
     document.querySelector('input').addEventListener('keydown', function(e) {
-        if (e.KeyCode === 13) {
-            console.log('13')
+        if (e.keyCode === 13) {
+            let inputOut = this.value
+            plusList(inputOut)
         }
     })
-}
-enterKey()
+})()
+function plusList(n) {
+    let text = '<li class="block"> <i class="fa fa-circle-thin" aria-hidden="true"></i><p class="text">'+n+'</p><i class="fa fa-trash" aria-hidden="true"></i></li>'
+    const list = document.getElementById('list')
+    list.insertAdjacentHTML('beforeend', text)
+    document.querySelector('input').value = ''
+    }
